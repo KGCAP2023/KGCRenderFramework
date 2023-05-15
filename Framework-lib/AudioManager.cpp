ad1e066cf7af19b697bb9ee5162519d5d606ef27
djl987645 <djl987645@gmail.com>
2023-05-15 21:38:35 +0900

    오디오 매니저 파일 및 맵 내부 존재시 작동으로 수정

--- Framework-lib/AudioManager.cpp
+++ Framework-lib/AudioManager.cpp
@@ -2,6 +2,8 @@
 #include "AudioManager.h"
 #include "Framework.h"
 
+
+
 //잠재적 오류사항
 // 오디오 33개 동시 재생시 32번째 재생한 오디오 pop and 33번째가 push 됨(스택)
 // 
@@ -22,21 +24,27 @@ bool AudioManager::Initialize(Framework* framework)
 
 void AudioManager::LoadAudio(const char* audioName, const char* audioFilePath) 
 {
-	if (res->m_sound.find(audioName) == res->m_sound.end())
+	if (_access(audioFilePath, 0) != false) 
 	{
-		FMOD::Sound* sound;
-		this->m_system->createSound(audioFilePath, FMOD_DEFAULT, nullptr, &sound);
-		res->m_sound.insert(std::make_pair<>(audioName, sound));
-		this->m_channel[audioName]->setVolume(1.0f);
+		if (res->m_sound.find(audioName) == res->m_sound.end())
+		{
+			FMOD::Sound* sound;
+				this->m_system->createSound(audioFilePath, FMOD_DEFAULT, nullptr, &sound);
+				res->m_sound.insert(std::make_pair<>(audioName, sound));
+				this->m_channel[audioName]->setVolume(1.0f);
+		}
+		else
+			std::cout << "Audio Name already exist" << std::endl;
 	}
-	else
-		std::cout << "Audio Name already exist" << std::endl;
+	else 
+		std::cout << "Audio File NOT exist" << std::endl;
 }
 
 
 void AudioManager::DeleteAudio(const char* audioName)
 {
-	res->m_sound.erase(audioName);
+	if(res->m_sound.find(audioName) != res->m_sound.end())
+		res->m_sound.erase(audioName);
 }
 
 
@@ -44,17 +52,20 @@ void AudioManager::DeleteAudio(const char* audioName)
 
 void AudioManager::PlayAudio(const char* audioName)
 {
+	if (res->m_sound.find(audioName) != res->m_sound.end())
 		this->m_system->playSound(res->m_sound[audioName], nullptr, false, &m_channel[audioName]);
 }
 
 
 void AudioManager::StopAudio(const char* audioName)
 {
+	if(this->m_channel.find(audioName) != this->m_channel.end())
 		this->m_channel[audioName]->stop();
 }
 
 void AudioManager::PauseAudio(const char* audioName)
 {
+	if (this->m_channel.find(audioName) != this->m_channel.end())
 		this->m_channel[audioName]->setPaused(true);
 }
 
@@ -63,6 +74,7 @@ void AudioManager::PauseAudio(const char* audioName)
 
 void AudioManager::ResumeAudio(const char* audioName)
 {
+	if (this->m_channel.find(audioName) != this->m_channel.end())
 		this->m_channel[audioName]->setPaused(false);
 }
 
@@ -70,5 +82,6 @@ void AudioManager::ResumeAudio(const char* audioName)
 
 void AudioManager::SetVolume(const char* audioName, float volume)
 {
+	if (this->m_channel.find(audioName) != this->m_channel.end())
 		this->m_channel[audioName]->setVolume(volume);
 }

ca82d95f67212919b5ffaf40dd1110316349dd68
구동혁 <dhku@naver.com>
2023-05-15 21:32:45 +0900

    Logger 수정

--- External/Includes/Framework/Logger.h
+++ External/Includes/Framework/Logger.h
@@ -4,9 +4,22 @@
 class Logger
 {
 public:
+	Logger()
+	{
+		logBuffer.reserve(500);
+	}
+
 	static void Log(std::string message);
 	static void Log(HRESULT hr, std::string message);
 	static void Log(std::wstring message);
 	static void Log(HRESULT hr, std::wstring message);
+
+	static void AddLog(std::string log)
+	{
+		logBuffer.push_back(log);
+	}
+
+	static std::vector<std::string>& GetLogBuffer() { return logBuffer; };
+	static std::vector<std::string> logBuffer;
 };
 
